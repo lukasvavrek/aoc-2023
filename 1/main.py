0d06b8715ba4ecb72919c0f28d1e0f109d1ab293
Lukas Vavrek <lvavrek7@gmail.com>
2023-12-01 19:55:55 +0100

    1-2: Python.

--- 1/main.py
+++ 1/main.py
@@ -2,22 +2,57 @@
 
 import sys
 
-total = 0
+map = {
+    'one': "1",
+    'two': "2",
+    'three': "3",
+    'four': "4",
+    'five': "5",
+    'six':  "6",
+    'seven': "7",
+    'eight': "8",
+    'nine': "9",
+}
 
-for line in sys.stdin.readlines():
-    d = 0
+def process_line(line):
+    i = 0
+    while i < len(line):
+        if line[i:i+3] in map.keys():
+            yield map[line[i:i+3]]
+            i += 2
+        elif line[i:i+4] in map.keys():
+            yield map[line[i:i+4]]
+            i += 3
+        elif line[i:i+5] in map.keys():
+            yield map[line[i:i+5]]
+            i += 4
+        else:
+            yield line[i]
+            i += 1
 
+def first_number(line):
     for ch in line:
         if ch.isnumeric():
-            d = 10 * int(ch)
-            break
+            return int(ch)
 
-    for ch in line[::-1]:
-        if ch.isnumeric():
-            d += int(ch)
-            break
+    return 0
 
-    total += d
+t1 = 0
+t2 = 0
 
-print(total)
+for line in sys.stdin.readlines():
+    d = 10 * first_number(line)
+    d += first_number(line[::-1])
+
+    t1 += d
+
+    line = ''.join(process_line(line.strip().lower()))
+
+    d = 10 * first_number(line)
+    d += first_number(line[::-1])
+
+    t2 += d
+
+print(t1)
+print(t2)
 
