75349624338ab0df8aca1e7c7489d88d17e536cf
Lukas Vavrek <lvavrek7@gmail.com>
2023-12-04 15:53:46 +0100

    3-2: Python.

--- 3/main.py
+++ 3/main.py
@@ -74,7 +74,7 @@ def print_lines(lines):
     print('====================')
 
 
-def is_part_number(number, lines):
+def get_border(number, lines):
     x1 = number.sidx-1
     x2 = number.sidx + len(number)
 
@@ -85,6 +85,12 @@ def is_part_number(number, lines):
     border += lines[number.yidx][x2]
 
     print(f'{number} x1,x2=({x1}, {x2}) border = {border}')
+    return border
+
+
+def is_part_number(number, lines):
+    border = get_border(number, lines)
+
     border = filter(lambda x: not x.isdigit(), border)
     return not all(map(lambda x: x == '.', border))
 
@@ -94,3 +100,35 @@ print_lines(lines)
 part_numbers = filter(lambda x: is_part_number(x, lines), numbers)
 part_numbers = map(lambda x: x.value, part_numbers)
 print(sum(part_numbers))
+
+
+def has_border_on(number, lines, x, y):
+    x1 = number.sidx-1
+    x2 = number.sidx + len(number)
+
+    if y == number.yidx - 1 and x1 <= x <= x2:
+        return True
+    if y == number.yidx + 1 and x1 <= x <= x2:
+        return True
+
+    if y == number.yidx and x == x1:
+        return True
+    if y == number.yidx and x == x2:
+        return True
+
+    return False
+
+
+gears = []
+
+for y in range(len(lines)):
+    for x in range(len(lines[y])):
+        if lines[y][x] == '*':
+            neighbors = filter(lambda number: has_border_on(number, lines, x, y), numbers)
+            neighbors = list(neighbors)
+
+            if len(neighbors) == 2:
+                print(f'Gear at {x}, {y} => {neighbors[0]} and {neighbors[1]}')
+                gears.append((neighbors[0].value, neighbors[1].value))
+
+print(sum(map(lambda x: x[0] * x[1], gears)))
