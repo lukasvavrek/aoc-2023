6b83997808bace8ce34e22fc5a4fab91d801be1d
Lukas Vavrek <lvavrek7@gmail.com>
2023-12-04 16:07:14 +0100

    Small refactoring.

--- 3/main.py
+++ 3/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 # 533775
-#
+# 78236071
 
 import sys
 
@@ -21,6 +21,40 @@ class Number:
 
     def __repr__(self):
         return f"({self.value}, yidx: {self.yidx}, sidx: {self.sidx}, len: {len(self)})"
+
+    def get_border(self, lines):
+        x1 = self.sidx-1
+        x2 = self.sidx + len(self)
+
+        border = ''
+        border += lines[self.yidx - 1][x1: x2+1]
+        border += lines[self.yidx + 1][x1: x2+1]
+        border += lines[self.yidx][x1]
+        border += lines[self.yidx][x2]
+
+        return border
+
+    def is_part_number(self, lines):
+        border = self.get_border(lines)
+
+        border = filter(lambda x: not x.isdigit(), border)
+        return not all(map(lambda x: x == '.', border))
+
+    def has_border_on(self, lines, x, y):
+        x1 = self.sidx-1
+        x2 = self.sidx + len(self)
+
+        if y == self.yidx - 1 and x1 <= x <= x2:
+            return True
+        if y == self.yidx + 1 and x1 <= x <= x2:
+            return True
+
+        if y == self.yidx and x == x1:
+            return True
+        if y == self.yidx and x == x2:
+            return True
+
+        return False
 
 
 def extract_numbers(line, yidx):
@@ -66,57 +100,9 @@ numbers = filter(lambda x: len(x) > 0, numbers)
 numbers = flatten(numbers)
 numbers = list(numbers)
 
-
-def print_lines(lines):
-    print('====================')
-    for line in lines:
-        print(line)
-    print('====================')
-
-
-def get_border(number, lines):
-    x1 = number.sidx-1
-    x2 = number.sidx + len(number)
-
-    border = ''
-    border += lines[number.yidx - 1][x1: x2+1]
-    border += lines[number.yidx + 1][x1: x2+1]
-    border += lines[number.yidx][x1]
-    border += lines[number.yidx][x2]
-
-    print(f'{number} x1,x2=({x1}, {x2}) border = {border}')
-    return border
-
-
-def is_part_number(number, lines):
-    border = get_border(number, lines)
-
-    border = filter(lambda x: not x.isdigit(), border)
-    return not all(map(lambda x: x == '.', border))
-
-
-print_lines(lines)
-
-part_numbers = filter(lambda x: is_part_number(x, lines), numbers)
+part_numbers = filter(lambda x: x.is_part_number(lines), numbers)
 part_numbers = map(lambda x: x.value, part_numbers)
 print(sum(part_numbers))
-
-
-def has_border_on(number, lines, x, y):
-    x1 = number.sidx-1
-    x2 = number.sidx + len(number)
-
-    if y == number.yidx - 1 and x1 <= x <= x2:
-        return True
-    if y == number.yidx + 1 and x1 <= x <= x2:
-        return True
-
-    if y == number.yidx and x == x1:
-        return True
-    if y == number.yidx and x == x2:
-        return True
-
-    return False
 
 
 gears = []
@@ -124,11 +110,10 @@ gears = []
 for y in range(len(lines)):
     for x in range(len(lines[y])):
         if lines[y][x] == '*':
-            neighbors = filter(lambda number: has_border_on(number, lines, x, y), numbers)
+            neighbors = filter(lambda number: number.has_border_on(lines, x, y), numbers)
             neighbors = list(neighbors)
 
             if len(neighbors) == 2:
-                print(f'Gear at {x}, {y} => {neighbors[0]} and {neighbors[1]}')
                 gears.append((neighbors[0].value, neighbors[1].value))
 
 print(sum(map(lambda x: x[0] * x[1], gears)))
