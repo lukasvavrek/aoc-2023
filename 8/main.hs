82b7883b8b1eb395cc4e20ff2251ee2c1f4d59aa
Lukas Vavrek <lvavrek7@gmail.com>
2023-12-10 16:05:17 +0100

    8-2: Haskell (WIP - stack overflow).

--- 8/main.hs
+++ 8/main.hs
@@ -4,18 +4,20 @@
    --package split
    --package text
    --package containers
+   --package strict
 -}
 
-import Data.Map qualified as Map
+import Data.List (all, isSuffixOf, unfoldr)
+import Data.Map.Strict qualified as Map
 import Data.Maybe (catMaybes)
 
 data RLMap = RLMap {left :: String, right :: String} deriving (Show)
 
 data Problem = Problem
-  { rlMap :: Map.Map String RLMap,
-    instructions :: String,
+  { instructions :: String,
     start :: String,
-    end :: String
+    end :: String,
+    acc :: Int
   }
   deriving (Show)
 
@@ -27,11 +29,49 @@ main = do
 
   let rlMap = Map.fromList $ catMaybes $ map parseRLMap $ drop 2 $ lines input
 
-  let problem = Problem rlMap instructions "AAA" "ZZZ"
+  let problem = Problem instructions "AAA" "ZZZ" 0
+  print $ solver rlMap [problem]
 
-  print $ solver problem
+  -- Pt. 2
+  let starts = filter (isSuffixOf "A") $ Map.keys rlMap
+  let problems = map (\start -> Problem instructions start "Z" 0) starts
+  print $ solver rlMap problems
 
   return ()
+
+solver :: Map.Map String RLMap -> [Problem] -> Int
+solver _ [] = error "No problems to solve"
+solver table problems =
+  maximum $ map acc $ until allSolved (map (step table)) problems
+  where
+    allSolved = all isSolved
+
+solver' :: Map.Map String RLMap -> [Problem] -> Int
+solver' _ [] = error "No problems to solve"
+solver' table problems =
+  if allSolved
+    then acc $ head problems
+    else solver' table problems' 
+  where
+    allSolved = all isSolved problems
+    problems' = map (step table) problems
+
+step :: Map.Map String RLMap -> Problem -> Problem
+step table problem = problem {instructions = instructions', start = start', acc = acc'}
+  where
+    instr = head $ instructions problem
+    instructions' = drop 1 $ instructions problem
+    acc' = acc problem + 1
+    kv = Map.lookup (start problem) table
+    start' = case kv of
+      Nothing -> error "Invalid start key"
+      Just rlMap -> case instr of
+        'L' -> left rlMap
+        'R' -> right rlMap
+        _ -> error "Invalid instruction"
+
+isSolved :: Problem -> Bool
+isSolved problem = end problem `isSuffixOf` start problem
 
 parseRLMap :: String -> Maybe (String, RLMap)
 parseRLMap line = do
@@ -49,22 +89,3 @@ parseString input =
    in case parts of
         [aaa, bbb, ccc] -> (aaa, bbb, ccc)
         _ -> error "Invalid input format"
-
-solver :: Problem -> Int
-solver problem = solver' problem 0
-
-solver' :: Problem -> Int -> Int
-solver' problem acc
-  | start problem == end problem = acc
-  | otherwise =
-      solver' problem {instructions = instructions', start = start'} acc + 1
-  where
-    instr = head $ instructions problem
-    instructions' = drop 1 $ instructions problem
-    kv = Map.lookup (start problem) (rlMap problem)
-    start' = case kv of
-      Nothing -> error "Invalid start key"
-      Just rlMap -> case instr of
-        'L' -> left rlMap
-        'R' -> right rlMap
-        _ -> error "Invalid instruction"
